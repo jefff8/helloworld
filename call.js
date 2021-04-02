b47d204fc75635e030c735c25a986556117f7270
Jeff_8 <360531371@qq.com>
2021-04-02 15:06:50 +0800

    apply、bind、call 改变执行作用域。

--- call.js
+++ call.js
@@ -1,23 +1,22 @@
 /**
- * 手写apply
+ * 手写call
  */
 
-Function.prototype.myApply = function (context = globalThis) {
+Function.prototype.myCall = function (context = globalThis) {
   // 关键步骤，在 context 上调用方法，触发 this 绑定为 context，使用 Symbol 防止原有属性的覆盖
   const key = Symbol('key')
+
   context[key] = this
-  let res
-  if (arguments[1]) {
-    res = context[key](...arguments[1])
-  } else {
-    res = context[key]()
-  }
+  
+  // es5 可通过 for 遍历 arguments 得到参数数组
+  const args = [...arguments].slice(1)
+  const res = context[key](...args)
   delete context[key]
   return res
 }
 
 const me = {
-  name: 'Apply'
+  name: 'Call'
 }
 
 function say() {
@@ -25,6 +24,6 @@ function say() {
   console.log(`My name is ${this.name || 'default'}`)
 }
 
-say()
-say.myApply(me)
 
+say()
+say.myCall(me)
